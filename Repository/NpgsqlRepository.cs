8559e465682c08f5f360aa3540d02fab3233d5d6
meyerrj <meyerrj@rayonnant.net>
2012-12-05 02:40:48 +0000

    Npgsql Repository Install-Package Npgsql

--- Repository/NpgsqlRepository.cs
+++ Repository/NpgsqlRepository.cs
@@ -9,59 +9,34 @@ using Miata.Library.Translator;
 using Npgsql;
 using NpgsqlTypes;
 
+/*
+ * Install-Package Npgsql
+ */
+
 public abstract class NpgsqlRepository<T> : AbstractRepository<T>
+{
+	// Logging instance
+	private static readonly ILog log = LogManager.GetLogger(typeof(NpgsqlRepository<T>));
+
+	private string dataSourceName = ConfigurationManager.AppSettings["db.connection.name"].ToString();
+
+	public override IDbConnection GetConnection()
 	{
-		// Logging instance
-		private static readonly ILog log = LogManager.GetLogger(typeof(NpgsqlRepository<T>));
-
-		private string dataSourceName = ConfigurationManager.AppSettings["db.connection.name"].ToString();
-
-		public override IDbConnection GetConnection()
+		if (null == this.DbConnection)
 		{
-			if (null == this.DbConnection)
-			{
-				string connectionString = ConfigurationManager.ConnectionStrings[dataSourceName].ConnectionString;
-				this.DbConnection = new NpgsqlConnection(connectionString);
-			}
-			return this.DbConnection;
+			string connectionString = ConfigurationManager.ConnectionStrings[dataSourceName].ConnectionString;
+			this.DbConnection = new NpgsqlConnection(connectionString);
 		}
+		return this.DbConnection;
+	}
 
-		/*
-		protected IEnumerable<TRowType> ProcessRefCursor<TRowType>(OracleRefCursor cursor)
+	/*
+	protected IEnumerable<TRowType> ProcessRefCursor<TRowType>(OracleRefCursor cursor)
+	{
+		IEnumerable<TRowType> results = null;
+		try
 		{
-			IEnumerable<TRowType> results = null;
-			try
-			{
-				NpgsqlDataReader reader = (NpgsqlDataReader)cursor.GetDataReader();
-				try
-				{
-					BaseTranslator<TRowType> oTranslator = new BaseTranslator<TRowType>();
-					results = oTranslator.ParseReader(reader);
-				}
-				catch (Exception e)
-				{
-					log.Error(e.Message, e);
-					throw e;
-				}
-				finally
-				{
-					if (reader != null)
-					{
-						reader.Close();
-					}
-				}
-			}
-			catch (Exception)
-			{
-				throw;
-			}
-			return results;
-		}
-		*/
-
-		protected IEnumerable<TRowType> ProcessReader<TRowType>(IDataReader reader)
-		{
-			IEnumerable<TRowType> results = null;
+			NpgsqlDataReader reader = (NpgsqlDataReader)cursor.GetDataReader();
 			try
 			{
 				BaseTranslator<TRowType> oTranslator = new BaseTranslator<TRowType>();
@@ -79,78 +54,107 @@ public abstract class NpgsqlRepository<T> : AbstractRepository<T>
 					reader.Close();
 				}
 			}
-			return results;
 		}
-
-		protected dynamic ProcessOutputParameters(NpgsqlCommand cmd)
+		catch (Exception)
 		{
-			dynamic expando = new ExpandoObject();
-			var expandoDict = expando as IDictionary<String, object>;
-
-			List<NpgsqlParameter> outParameters = new List<NpgsqlParameter>();
-			foreach (NpgsqlParameter p in cmd.Parameters)
-			{
-				if (!p.Direction.Equals(ParameterDirection.Input))
-				{
-					outParameters.Add(p);
-				}
-			}
-
-			foreach (var item in outParameters)
-			{
-				NpgsqlParameter cmdParameter = cmd.Parameters[item.ParameterName];
-				NpgsqlDbType cmdParameterType = cmdParameter.NpgsqlDbType;
-				String cmdParameterName = cmdParameter.ParameterName;
-				if (NpgsqlDbType.Refcursor.Equals(cmdParameterType))
-				{
-					using (NpgsqlDataReader cursor = (NpgsqlDataReader)cmdParameter.Value)
-					{
-						log.WarnFormat("REF CURSORS are not supported by this method.  Use ProcessOutputParameters<TRowType>(OracleCommand cmd) instead to retrieve the value of {0}", cmdParameterName);
-					}
-				}
-				else
-				{
-					expandoDict[cmdParameterName] = cmdParameter.Value;
-				}
-			}
-
-			this.DbConnection.Close();
-
-			return expando;
+			throw;
 		}
-
-		protected dynamic ProcessOutputParameters<TRowType>(NpgsqlCommand cmd)
-		{
-			dynamic expando = new ExpandoObject();
-			var expandoDict = expando as IDictionary<String, object>;
-
-			List<NpgsqlParameter> outParameters = new List<NpgsqlParameter>();
-			foreach (NpgsqlParameter p in cmd.Parameters)
-			{
-				if (!p.Direction.Equals(ParameterDirection.Input))
-				{
-					outParameters.Add(p);
-				}
-			}
-
-			foreach (var item in outParameters)
-			{
-				NpgsqlParameter cmdParameter = cmd.Parameters[item.ParameterName];
-				NpgsqlDbType cmdParameterType = cmdParameter.NpgsqlDbType;
-				String cmdParameterName = cmdParameter.ParameterName;
-				if (NpgsqlDbType.Refcursor.Equals(cmdParameterType))
-				{
-					using (NpgsqlDataReader cursor = (NpgsqlDataReader)cmdParameter.Value)
-					{
-						expandoDict[cmdParameterName] = this.ProcessReader<TRowType>(cursor);
-					}
-				}
-				else
-				{
-					expandoDict[cmdParameterName] = cmdParameter.Value;
-				}
-			}
-
-			return expando;
-		}
+		return results;
 	}
+	*/
+
+	protected IEnumerable<TRowType> ProcessReader<TRowType>(IDataReader reader)
+	{
+		IEnumerable<TRowType> results = null;
+		try
+		{
+			BaseTranslator<TRowType> oTranslator = new BaseTranslator<TRowType>();
+			results = oTranslator.ParseReader(reader);
+		}
+		catch (Exception e)
+		{
+			log.Error(e.Message, e);
+			throw e;
+		}
+		finally
+		{
+			if (reader != null)
+			{
+				reader.Close();
+			}
+		}
+		return results;
+	}
+
+	protected dynamic ProcessOutputParameters(NpgsqlCommand cmd)
+	{
+		dynamic expando = new ExpandoObject();
+		var expandoDict = expando as IDictionary<String, object>;
+
+		List<NpgsqlParameter> outParameters = new List<NpgsqlParameter>();
+		foreach (NpgsqlParameter p in cmd.Parameters)
+		{
+			if (!p.Direction.Equals(ParameterDirection.Input))
+			{
+				outParameters.Add(p);
+			}
+		}
+
+		foreach (var item in outParameters)
+		{
+			NpgsqlParameter cmdParameter = cmd.Parameters[item.ParameterName];
+			NpgsqlDbType cmdParameterType = cmdParameter.NpgsqlDbType;
+			String cmdParameterName = cmdParameter.ParameterName;
+			if (NpgsqlDbType.Refcursor.Equals(cmdParameterType))
+			{
+				using (NpgsqlDataReader cursor = (NpgsqlDataReader)cmdParameter.Value)
+				{
+					log.WarnFormat("REF CURSORS are not supported by this method.  Use ProcessOutputParameters<TRowType>(OracleCommand cmd) instead to retrieve the value of {0}", cmdParameterName);
+				}
+			}
+			else
+			{
+				expandoDict[cmdParameterName] = cmdParameter.Value;
+			}
+		}
+
+		this.DbConnection.Close();
+
+		return expando;
+	}
+
+	protected dynamic ProcessOutputParameters<TRowType>(NpgsqlCommand cmd)
+	{
+		dynamic expando = new ExpandoObject();
+		var expandoDict = expando as IDictionary<String, object>;
+
+		List<NpgsqlParameter> outParameters = new List<NpgsqlParameter>();
+		foreach (NpgsqlParameter p in cmd.Parameters)
+		{
+			if (!p.Direction.Equals(ParameterDirection.Input))
+			{
+				outParameters.Add(p);
+			}
+		}
+
+		foreach (var item in outParameters)
+		{
+			NpgsqlParameter cmdParameter = cmd.Parameters[item.ParameterName];
+			NpgsqlDbType cmdParameterType = cmdParameter.NpgsqlDbType;
+			String cmdParameterName = cmdParameter.ParameterName;
+			if (NpgsqlDbType.Refcursor.Equals(cmdParameterType))
+			{
+				using (NpgsqlDataReader cursor = (NpgsqlDataReader)cmdParameter.Value)
+				{
+					expandoDict[cmdParameterName] = this.ProcessReader<TRowType>(cursor);
+				}
+			}
+			else
+			{
+				expandoDict[cmdParameterName] = cmdParameter.Value;
+			}
+		}
+
+		return expando;
+	}
+}
