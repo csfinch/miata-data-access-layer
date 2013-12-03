ddc730e29fa52e8798a8cf6c1cecf8d609e01339
admin@markhansen.info <admin@markhansen.info@d3e81c18-e1bd-66ec-e64b-a215ef5567ff>
2013-12-03 22:48:07 +0000

    Cleanup and missing test objects

---
+++ trunk/MiataLibrary.Tests/TestObjects/TestObject.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Linq.Mapping;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MiataLibrary.Tests.TestObjects
+{
+    internal class TestObject
+    {
+        [Column(Name="Id")]
+        public int Id { get; set; }
+
+        [Column(Name = "Description")]
+        public string Description { get; set; }
+
+        [Column(Name = "CreatedDate")]
+        public DateTime CreatedDate { get; set; }
+
+        [Column(Name = "Status")]
+        public string Status { get; set; }
+    }
+}
